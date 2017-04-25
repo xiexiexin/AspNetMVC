260635c0687979fb4b6992c9044f8f4289fdc3f5
xundh <xundh@qq.com>
2017-04-25 22:34:58 +0800

    init

---
+++ MvcApplication1/MvcApplication1/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
